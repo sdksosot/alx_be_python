fe0206ecef5e22a15465779f7424e023ff13122d
sdksosot <aweal5834@gmail.com>
2024-12-16 04:20:07 -0800

    Create basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+# إسناد القيم إلى المتغيرات
+number1 = 10
+number2 = 5
+
+# إجراء العمليات الحسابية
+sum_result = number1 + number2
+difference_result = number1 - number2
+product_result = number1 * number2
+
+# طباعة النتائج
+print(f"جمع {number1} و {number2} هو {sum_result}")
+print(f"طرح {number1} و {number2} هو {difference_result}")
+print(f"ضرب {number1} و {number2} هو {product_result}")
