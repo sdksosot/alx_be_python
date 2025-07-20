2100891a4b7b0d48f36559dbb7634cb3f902984e
sdksosot <aweal5834@gmail.com>
2025-07-20 20:36:29 +0300

    Update pattern_drawing.py

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,8 +1,8 @@
-size = int(input("Enter the size of the square: "))
+user = int(input("Enter the size of the square: "))
 
 row = 0
 while row < size:
-    for col in range(size):
+    for col in range(user):
         print("*", end="")
     print()  # Move to the next line after each row
     row += 1
