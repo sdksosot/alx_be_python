f442d36b70f668c5ac741ed9988c3e52c0a88b84
sdksosot <aweal5834@gmail.com>
2025-07-26 01:26:36 +0300

    Update shopping_list_manager.py

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -26,18 +26,18 @@ def main():
     shopping_list = []
     while True:
         display_menu()
-        choice = input("Enter your choice: ")
+        choice = int(input("Enter your choice: "))
 
-        if choice == '1':
+        if choice == 1:
             
             adding(shopping_list)
-        elif choice == '2':
+        elif choice == 2:
             remove(shopping_list)
             pass
-        elif choice == '3':
+        elif choice == 3:
             show(shopping_list)
             pass
-        elif choice == '4':
+        elif choice == 4:
             print("Goodbye!")
             break
         else:
