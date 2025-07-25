0c16d85d924aa30bc9ed66a50667bf7becd14f3a
sdksosot <aweal5834@gmail.com>
2025-07-26 01:24:53 +0300

    Add files via upload

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,48 @@
+
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+def adding (shopping_list):
+    user = input ("add your item: ")
+    shopping_list.append(user)
+    print('user added succsefully!')
+def remove(shopping_list):
+    user = input("what object you want to remove from the cart :")
+    if user in shopping_list:
+        shopping_list.remove(user)
+    else:
+        print('this item isnot in the shopping list')
+def show(shopping_list):
+    if not shopping_list:
+        print('the shopping list is empty')
+    else:
+        
+        for item in shopping_list:
+            print(item)
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            
+            adding(shopping_list)
+        elif choice == '2':
+            remove(shopping_list)
+            pass
+        elif choice == '3':
+            show(shopping_list)
+            pass
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
+
