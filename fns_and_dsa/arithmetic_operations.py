b66946c06b850ed0768bff4893f29f878b28189d
sdksosot <aweal5834@gmail.com>
2025-07-26 01:09:42 +0300

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,15 +1,14 @@
-def perform_operation(num1:float , num2:float , operation: str ):
-  if operation == '+':
-     return num1 + num2
-  elif operation == '-':
-     return num1 - num2
-  elif operation == '/':
-     if num2 != 0:
+def perform_operation(num1, num2, operation):
+    if operation == "add":
+        return num1 + num2
+    elif operation == "subtract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+        if num2 == 0:
+            return "Error: Division by zero"
         return num1 / num2
-     else:
-        return 'cannot devied by zero'
-  elif operation == '*':
-     return num1 * num2
-  else:
-     return 'invalid value'
-  
+    else:
+        return "Error: Invalid operation"
+
