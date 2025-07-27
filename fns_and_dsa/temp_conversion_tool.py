7437f459e9f085c6fb157083cf2db3f61f062e65
sdksosot <aweal5834@gmail.com>
2025-07-27 03:19:58 +0300

    Update temp_conversion_tool.py

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,12 +1,15 @@
-FAHRENHEIT_TO_CELSIUS_FACTOR = float(input("Enter the temperature to convert: "))
-f_c = input('Is this temperature in Celsius or Fahrenheit? (C/F): ').lower()
+FAHRENHEIT_TO_CELSIUS_FACTOR = (5/9)
+CELSIUS_TO_FAHRENHEIT_FACTOR = (9/5) 
 def convert_to_celsius(fahrenheit):
-    return  (fahrenheit - 32) * (5/9)
+    return (fahrenheit - 32) * 5/9
 def convert_to_fahrenheit(celsius):
-    return  (celsius * (9/5)) + 32
-if f_c == 'c':
-    print(f'{FAHRENHEIT_TO_CELSIUS_FACTOR}°F is {convert_to_celsius(FAHRENHEIT_TO_CELSIUS_FACTOR)}°C')
-elif f_c == 'f':
-    print(f'{FAHRENHEIT_TO_CELSIUS_FACTOR}°C is {convert_to_fahrenheit(FAHRENHEIT_TO_CELSIUS_FACTOR)}°F')
-else:
-    print("invalid temperature. Please enter a numeric value")
+    return (celsius*9/5)+32
+temp = float(input("Enter the temperature to convert: "))
+t_c = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").lower()
+if t_c == 'c':
+    print(f'{temp}°C is {convert_to_fahrenheit(temp)}°F')
+elif t_c == 'f':
+    print(f'{temp}°C is {convert_to_celsius(temp)}°F')
+elif not int(temp) or temp != 'c' or temp!='f':
+    print("Invalid temperature. Please enter a numeric value.”")
+
