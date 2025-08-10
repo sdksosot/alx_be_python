824bdd60087693437ca3221ca3f64b5b5a7c2cdb
sdksosot <aweal5834@gmail.com>
2025-08-10 22:16:40 +0300

    Add files via upload

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,16 @@
+class Shape:
+    def area(self):
+        raise ' NotImplementedError'
+class Rectangle(Shape):
+    def __init__(self,length,width):
+        super().__init__()
+        self.length = length
+        self.width = width
+    def area(self):
+        return self.length*self.width
+class Circle(Shape):
+    def __init__(self,radius):
+        super().__init__()
+        self.radius = radius
+    def area(self):
+        return (22/7)*(self.radius*self.radius)
